0aa4122d8891f944d47eff78d02ee0cfc09c58f5
awul <ilhamnopi@icloud.com>
2025-02-26 23:01:13 +0700

    feat: Slicing Detail Product section and Sugestion product

--- src/pages/productdetail.jsx
+++ src/pages/productdetail.jsx
@@ -3,6 +3,8 @@ import Footer from '../parts/Footer';
 import Header from '../parts/Header/index';
 import SiteMap from '../parts/SiteMap';
 import Breadcrumb from '../components/Breadcrumb/index';
+import SugestionProduct from '../parts/ProductDetailPage/SugestionProduct/index';
+import DetailProduct from '../parts/ProductDetailPage/DetailProduct/index';
 
 
 const Productdetail = () => {
@@ -14,6 +16,8 @@ const Productdetail = () => {
                 { url: "/categories/91231", name: "Office Room" },
                 { url: "/categories/91231/products/7888", name: "Details" },
             ]} />
+            <DetailProduct />
+            <SugestionProduct />
             <SiteMap />
             <Footer />
         </>
